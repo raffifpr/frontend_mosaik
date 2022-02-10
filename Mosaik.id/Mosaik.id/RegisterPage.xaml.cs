5049219ad7be2058f976f67683cba0fa7d3a7db5
kahfizulkifli <kahfizulkifli@students.itb.ac.id>
2022-02-10 07:42:07 +0700

    Tampilan Register

---
+++ Mosaik.id/Mosaik.id/RegisterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mosaik.id
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+        private void Sign_Up_Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Sign Up", "You have signed up!", "OK");
+        }
+
+    }
+}
