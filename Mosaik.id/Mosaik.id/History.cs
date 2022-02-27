6728423debf0c0cf6f8a410bb02da76b8d2e2f92
Raffi Fadhlurrahman <13519219@std.stei.itb.ac.id>
2022-02-27 12:18:15 +0000

    Upload New File

---
+++ Mosaik.id/Mosaik.id/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Mosaik.id
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Link { get; set; }
+        public string AccessedTime { get; set; }
+    }
+}
